5db0243e4adfae7eac3e3584e747c583d0bc9212
your_name <your_email@example.com>
2020-11-04 23:06:05 -0500

    commit tree 692fb6f217ec644e2892f2e82b9b0c7bffd711b0

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,40 @@
-# Add your code here
+require 'pry'
+class Dog
+    
+    
+    # attr_accessor 
+    attr_reader :name 
+    @@all = []
+   
+
+    def initialize(name)
+        @name = name 
+        #@@all << self 
+        save 
+        
+    end 
+
+    def self.all
+      @@all  
+    end 
+
+    def self.clear_all
+        @@all.clear
+    end 
+
+    def self.print_all
+
+          @@all.each do |dogs|
+            puts dogs.name 
+          end 
+        
+    end 
+
+    def save 
+        @@all << self 
+    end 
+     
+
+
+end 
+#binding.pry
